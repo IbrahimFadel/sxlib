698d121df6892eeec8271e301b26110b6ad5a8c6
Ibrahim Fadel <ibrahim.m.fadel@gmail.com>
2021-05-04 00:51:45 -0400

    delete old code

--- sxlib_api.h
+++ sxlib_api.h
@@ -6,7 +6,6 @@
 #include "sxlib_io.h"
 #include <stdlib.h>
 
-// sxlib_window_t *sxlib_create_window(sxlib_connection_t *c, u32 parent, u16 x, u16 y, u16 w, u16 h, u16 border_width, u16 group, u32 visual, u32 value_mask, u32 *value_list);
 sxlib_window_t *sxlib_create_window(sxlib_connection_t *c, u16 x, u16 y, u16 w, u16 h, u32 window_parent, u32 visual, u32 value_mask, u32 *value_list);
 void sxlib_map_window(sxlib_connection_t *c, sxlib_window_t *window);
 
--- sxlib_io.h
+++ sxlib_io.h
@@ -8,10 +8,4 @@
 int sxlib_send_request(sxlib_connection_t *c, const void *data, ssize_t size);
 int sxlib_read_reply(sxlib_connection_t *c, void *data, ssize_t size);
 
-// typedef struct iovec
-// {
-//     void *iov_base; /* Pointer to data.  */
-//     size_t iov_len; /* Length of data.  */
-// } iovec;
-
 #endif
